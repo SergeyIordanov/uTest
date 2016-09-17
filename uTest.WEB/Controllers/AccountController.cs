701c18cd0ebef4babd1864111a900d89b0810b68
iordanov.serhii <sergey.ior97@gmail.com>
2016-09-17 10:15:50 +0300

    implement account controller

---
+++ uTest.WEB/Controllers/AccountController.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections.Generic;
+using System.Security.Claims;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using uTest.Auth.BLL.DTO;
+using uTest.Auth.BLL.Infrastructure;
+using uTest.Auth.BLL.Interfaces;
+using uTest.WEB.Models;
+
+namespace uTest.WEB.Controllers
+{
+    public class AccountController : Controller
+    {
+        private IUserService UserService => 
+            HttpContext.GetOwinContext().GetUserManager<IUserService>();
+
+        private IAuthenticationManager AuthenticationManager => 
+            HttpContext.GetOwinContext().Authentication;
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(LoginModel model)
+        {
+            SetInitialData();
+            if (ModelState.IsValid)
+            {
+                var userDto = new UserDTO { Email = model.Email, Password = model.Password };
+                ClaimsIdentity claim = UserService.Authenticate(userDto);
+                if (claim == null)
+                {
+                    ModelState.AddModelError("", "Invalid login or password");
+                }
+                else
+                {
+                    AuthenticationManager.SignOut();
+                    AuthenticationManager.SignIn(new AuthenticationProperties
+                    {
+                        IsPersistent = true
+                    }, claim);
+
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            return View(model);
+        }
+
+        public ActionResult Logout()
+        {
+            AuthenticationManager.SignOut();
+            return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(RegisterModel model)
+        {
+            SetInitialData();
+            if (ModelState.IsValid)
+            {
+                var userDto = new UserDTO
+                {
+                    Email = model.Email,
+                    Password = model.Password,
+                    Name = model.Name,
+                    Role = "user"
+                };
+                OperationDetails operationDetails = UserService.Create(userDto);
+
+                if (!operationDetails.Succedeed)
+                {
+                    ModelState.AddModelError(operationDetails.Property, operationDetails.Message);
+                }
+                else
+                {
+                    return View("SuccessRegister");
+                }                 
+            }
+            return View(model);
+        }
+
+        private void SetInitialData()
+        {
+            UserService.SetInitialData(new UserDTO
+            {
+                Email = "admin@gmail.com",
+                UserName = "admin@gmail.com",
+                Password = "admin123",
+                Name = "Admin Adminovich",
+                Role = "admin",
+            }, new List<string> { "user", "admin", "manager", "blocked" });
+        }
+    }
+}
---
+++ uTest.WEB/Models/RegisterModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace uTest.WEB.Models
+{
+    public class RegisterModel
+    {
+        [Required(ErrorMessage = "Email is required")]
+        [RegularExpression(@"^.+@.+\..+$", ErrorMessage = "Wrong e-mail format")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [RegularExpression(@"^(?=.*[A-z])(?=.*\d)(?!.*\s)(?=^.{8,16}$).*$", ErrorMessage = "Password have to contain at least 1 digit character & letter. Length: 8 - 16")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Confirm password is required")]
+        [DataType(DataType.Password)]
+        [Compare("Password")]
+        public string ConfirmPassword { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+    }
+}
