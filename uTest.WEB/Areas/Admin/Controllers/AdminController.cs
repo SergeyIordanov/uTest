905ee9b6b6d2c6a16686492cf4625bb5914c0732
iordanov.serhii <sergey.ior97@gmail.com>
2016-09-18 14:29:06 +0300

    add admin area implement home page for admin area

---
+++ uTest.WEB/Areas/Admin/Controllers/AdminController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace uTest.WEB.Areas.Admin.Controllers
+{
+    public class AdminController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
